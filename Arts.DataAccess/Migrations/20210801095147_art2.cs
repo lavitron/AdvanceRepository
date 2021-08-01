4e1592fa52e20725b81487153993fa5c6aa40d5a
Ömer Furkan Doğruyol <omerfurkandogruyol@gmail.com>
2021-08-01 14:01:01 +0300

    Migration

---
+++ Arts.DataAccess/Migrations/20210801095147_art2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Arts.DataAccess.Migrations
+{
+    public partial class art2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Biography",
+                table: "Artists",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Biography",
+                table: "Artists",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
